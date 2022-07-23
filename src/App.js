e41aaf6c987db85640f0d02e8813bb4951454676
AdrianKausel <adrian.kausel@gmail.com>
2022-07-23 18:02:49 -0400

    Enter

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title"> Hello Dojo! </h1>
+        <h2> Things i need to do:</h2>
+        <div className="toDoList">
+          <li> Learn React </li>
+          <li> Climb Mt Everest </li>
+          <li> Run a marathon </li>
+          <li> Feed the dogs</li>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
